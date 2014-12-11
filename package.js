89ce7a4f5dcf1c9dd9ac4dc30128a0c14b742998
Peter Pavlovich <pavlovich@gmail.com>
2014-12-10 22:17:33 -0500

    Added famono instead of just famous.

--- package.js
+++ package.js
@@ -1,14 +1,15 @@
 Package.describe({
   name: 'sgi:famous-angular',
   summary: 'Famous-Angular packaged for use with Meteor apps',
-  version: '0.5.2',
+  version: '0.5.3',
   git: 'git@github.com:sginc/famous-angular.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.use('urigo:angular@0.5.7', 'client');
-  api.use('mjn:famous@0.3.1_2', 'client');
+  api.use('raix:famono@0.9.23', 'client');
+//  api.use('mjn:famous@0.3.1_2', 'client');
   api.addFiles('famous-angular.js', 'client');
   api.addFiles('famous-angular.css', 'client');
 });
