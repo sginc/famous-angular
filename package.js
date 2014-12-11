545044d156aeedecddd580bc67ee317035003ff5
Peter Pavlovich <pavlovich@gmail.com>
2014-12-10 22:55:49 -0500

    updated versions

--- package.js
+++ package.js
@@ -8,8 +8,8 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.use('urigo:angular@0.5.7', 'client');
-  api.use('raix:famono@0.9.23');
-//  api.use('mjn:famous@0.3.1_2', 'client');
+//  api.use('raix:famono@0.9.23');
+  api.use('mjn:famous@0.3.1_2', 'client');
   api.addFiles('famous-angular.js', 'client');
   api.addFiles('famous-angular.css', 'client');
 });
